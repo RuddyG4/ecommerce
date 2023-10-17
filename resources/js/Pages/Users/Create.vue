de97bfbdc34597c7297e25485c4ac196c272ee22
Gonzalo Q.H <ruddygonzqh@gmail.com>
2023-10-16 23:26:01 -0400

    Some modifications to Create user

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -28,7 +28,7 @@ function submit() {
 </script>
 
 <template>
-    <AppLayout title="Users">
+    <AppLayout title="Create user">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 Create user
